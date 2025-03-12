6ac6226955b8884a90eb969a1d971b534deb7717
이민서 <alstj7932@naver.com>
2025-03-12 14:51:53 +0900

    Delete zz_last_payment_backup3 directory

--- zz_last_payment_backup3/src/main/java/com/sboot/sijak/chart/service/PaymentService2.java
+++
@@ -1,8 +0,0 @@
-package com.sboot.sijak.chart.service;
-
-import java.util.Map;
-
-public interface PaymentService2 {
-    Map<String, Integer> getThreeMSales();
-    Map<String, Double> getRegionSales();
-}
